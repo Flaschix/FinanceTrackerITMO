c522ababf320b0c4ffeb0c021c6a6f0dcd9cb049
Flaschix <mishasigalov1@gmail.com>
2024-09-21 11:03:08 +0300

    Added CategoryViewModel and CategoryScreenState. Change return type of GetAllCategory function

--- app/src/main/java/com/example/financetrackeritmo/domain/usecase/GetAllCategoryUseCase.kt
+++ app/src/main/java/com/example/financetrackeritmo/domain/usecase/GetAllCategoryUseCase.kt
@@ -2,11 +2,12 @@ package com.example.financetrackeritmo.domain.usecase
 
 import com.example.financetrackeritmo.domain.entity.Category
 import com.example.financetrackeritmo.domain.repository.CategoryRepository
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class GetAllCategoryUseCase @Inject constructor(private val repository: CategoryRepository) {
-    suspend operator fun invoke(): StateFlow<List<Category>> {
+    suspend operator fun invoke(): SharedFlow<List<Category>> {
         return repository.getAllCategory()
     }
 }
