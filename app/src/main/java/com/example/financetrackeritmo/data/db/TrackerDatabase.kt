017d847f94c14e5be30be96fb0796dc8bf14bf82
Flaschix <mishasigalov1@gmail.com>
2024-09-26 13:12:13 +0300

    Fix bug with transaction and DB

--- app/src/main/java/com/example/financetrackeritmo/data/db/TrackerDatabase.kt
+++ app/src/main/java/com/example/financetrackeritmo/data/db/TrackerDatabase.kt
@@ -2,13 +2,16 @@ package com.example.financetrackeritmo.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.financetrackeritmo.data.dao.CategoryDao
 import com.example.financetrackeritmo.data.entity.CategoryDB
 import com.example.financetrackeritmo.data.dao.TransactionDao
+import com.example.financetrackeritmo.data.entity.ConvertersTransactionDate
 import com.example.financetrackeritmo.data.entity.TransactionDB
 
 //База данных. Сюда нужно будет добавить ещё транзакции (при каждом изменении бд, нужно менять его версию)
-@Database(entities = [CategoryDB::class, TransactionDB::class], version = 1)
+@Database(entities = [CategoryDB::class, TransactionDB::class], version = 2)
+@TypeConverters(ConvertersTransactionDate::class)
 abstract class TrackerDatabase: RoomDatabase() {
 
     abstract fun getCategoryDao(): CategoryDao
--- app/src/main/java/com/example/financetrackeritmo/data/entity/TransactionDB.kt
+++ app/src/main/java/com/example/financetrackeritmo/data/entity/TransactionDB.kt
@@ -2,13 +2,26 @@ package com.example.financetrackeritmo.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.time.LocalDate
+import androidx.room.TypeConverter
+import java.util.Date
 
 @Entity(tableName = "transactions")
 data class TransactionDB (
     @PrimaryKey(autoGenerate = true) val id: Long,
     val categoryId: Long,
     val amount: Double,
-    val date: LocalDate,
+    val date: Date,
     val note: String
 )
+
+class ConvertersTransactionDate {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time?.toLong()
+    }
+}
